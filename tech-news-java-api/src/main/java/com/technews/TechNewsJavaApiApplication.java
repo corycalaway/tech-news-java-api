4ac79c256fe55e52c9a807e8c2763f0e65609718
Cory Calaway <cory.c.calaway@gmail.com>
2021-06-07 18:19:52 -0600

    working through bug

--- tech-news-java-api/src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ tech-news-java-api/src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -1,13 +1,15 @@
 package com.technews;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class TechNewsJavaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechNewsJavaApiApplication.class, args);
+		System.out.println("Hello, World!");
 	}
 
 }
